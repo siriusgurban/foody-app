7f5f88f2df6d75c91e4ebdfa9d9dfd0307b9ae42
Nazifa16 <nazifa.abdullayeva@gmail.com>
2024-05-24 16:02:07 +0400

    the data of the offer has rendered and displayed on the cards

--- next.config.mjs
+++ next.config.mjs
@@ -4,40 +4,51 @@
 // import s from './next-i18next.configS';
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@mui/x-charts'],
+  transpilePackages: ["@mui/x-charts"],
   i18n: {
-    locales: ['en', 'az', 'de'],
-    defaultLocale: 'en',
+    locales: ["en", "az", "de"],
+    defaultLocale: "en",
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'gujarat.mallsmarket.com',
-        port: '',
-        pathname: '/sites/default/files/styles/medium/public/images/brands/**',
+        protocol: "https",
+        hostname: "gujarat.mallsmarket.com",
+        port: "",
+        pathname: "/sites/default/files/styles/medium/public/images/brands/**",
       },
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/v0/b/**',
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/**",
       },
       {
-        protocol: 'https',
-        hostname: 'media-cdn.tripadvisor.com',
-        port: '',
-        pathname: '/media/photo-p/**',
+        protocol: "https",
+        hostname: "media-cdn.tripadvisor.com",
+        port: "",
+        pathname: "/media/photo-p/**",
       },
       {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "hips.hearstapps.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-media0.fl.yelpcdn.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
 };
 
 export default nextConfig;
-
