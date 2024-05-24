0a7108f0b1d9ab5da1ba6092ef386ad90e1d5de1
siriusgurban <failoglu96@gmail.com>
2024-05-25 03:12:01 +0400

    client restaurant responsive done

--- shared/types/admin.ts
+++ shared/types/admin.ts
@@ -7,3 +7,26 @@ export type adminLogin = {
   email: string
   password: string
 }
+
+export interface Restaurant {
+  id: string
+  address: string
+  category_id: string
+  img_url: string
+  created: number // Assuming it represents a Unix timestamp
+  name: string
+  cuisine: string
+  delivery_min: number
+  delivery_price: number
+  products: Product[]
+}
+
+export interface Product {
+  id: string
+  img_url: string
+  price: number
+  created: number // Assuming it represents a Unix timestamp
+  name: string
+  description: string
+  rest_id: string
+}
