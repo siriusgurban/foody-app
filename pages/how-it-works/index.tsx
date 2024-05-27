afb4dc170e18bc5f2253ae367017e7691c7f04ce
Nazifa16 <nazifa.abdullayeva@gmail.com>
2024-05-27 22:42:02 +0400

    update

--- pages/how-it-works/index.tsx
+++ pages/how-it-works/index.tsx
@@ -1,15 +1,11 @@
 import ClientFooter from '@/shared/components/clientFooter'
 import ClientHeader from '@/shared/components/clientHeader'
-import UserBasket from '@/shared/components/userBasket'
-
 import React from 'react'
 
 const HowItWorks = () => {
     return (
         <>
             <ClientHeader />
-            <UserBasket />
-
             <ClientFooter />
         </>
     )
