ba3a600ebb905abc27f59323443d8d7938e6a75a
siriusgurban <failoglu96@gmail.com>
2024-05-29 19:19:54 +0400

    admin add product

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,8 +1,8 @@
 //@ts-nocheck
 
-import React, { useEffect, useState } from "react";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslation } from "next-i18next";
+import React, { useEffect, useState } from 'react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 import {
   Box,
   Button,
@@ -10,73 +10,73 @@ import {
   FormHelperText,
   Input,
   useToast,
-} from "@chakra-ui/react";
-import { useFormik } from "formik";
-import { useRouter } from "next/router";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getProducts, postProduct } from "../../../shared/services/products";
-import Head from "next/head";
-import AdminHeader from "@/shared/components/AdminHeader";
-import AdminAsideMenu from "@/shared/components/AdminAsideMenu";
+} from '@chakra-ui/react'
+import { useFormik } from 'formik'
+import { useRouter } from 'next/router'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getProducts, postProduct } from '../../../shared/services/products'
+import Head from 'next/head'
+import AdminHeader from '@/shared/components/AdminHeader'
+import AdminAsideMenu from '@/shared/components/AdminAsideMenu'
 
-import AdminProductsSide from "@/shared/components/adminProductsSide/AdminProductsSide";
-import AdminAsideMenuResponsive from "@/shared/components/AdminAsideMenuResponsive";
-import AdminSecondaryComponent from "@/shared/components/adminSecondaryComponent";
+import AdminProductsSide from '@/shared/components/adminProductsSide/AdminProductsSide'
+import AdminAsideMenuResponsive from '@/shared/components/AdminAsideMenuResponsive'
+import AdminSecondaryComponent from '@/shared/components/adminSecondaryComponent'
 
 const newPost = {
   img_url:
-    "https://firebasestorage.googleapis.com/v0/b/foodyapp-51b6e.appspot.com/o/product%2Fgeneral?alt=media&token=fc1915ed-481b-4f67-a018-7084e6df1aef",
-  price: "17.99",
-  name: "Nanamin Tandoori 9",
-  description: "Avtosh 1 manat chicken marinated in a blend of Indian spices.",
-  rest_id: "WFf6YBK5VPuxEFZE9FAR",
-};
+    'https://firebasestorage.googleapis.com/v0/b/foodyapp-51b6e.appspot.com/o/product%2Fgeneral?alt=media&token=fc1915ed-481b-4f67-a018-7084e6df1aef',
+  price: '17.99',
+  name: 'Nanamin Tandoori 9',
+  description: 'Avtosh 1 manat chicken marinated in a blend of Indian spices.',
+  rest_id: 'WFf6YBK5VPuxEFZE9FAR',
+}
 const initialValues = {
-  img_url: "",
-  price: "",
-  name: "",
-  description: "",
-  rest_id: "",
-};
+  img_url: '',
+  price: '',
+  name: '',
+  description: '',
+  rest_id: '',
+}
 
-function Login() {
-  const { t } = useTranslation("admin");
-  const toast = useToast();
-  const { push } = useRouter();
+function Products() {
+  const { t } = useTranslation('admin')
+  const toast = useToast()
+  const { push } = useRouter()
 
   const { values, handleChange, handleSubmit, errors } = useFormik({
     initialValues,
     onSubmit: (values) => mutate(values),
-  });
+  })
 
   const { mutate } = useMutation({
     mutationFn: postProduct,
     onSuccess(data, variables, context) {
-      console.log(data, "success");
+      console.log(data, 'success')
       toast({
-        title: "Account created.",
+        title: 'Account created.',
         description: "We've created your account for you.",
-        status: "success",
+        status: 'success',
         duration: 9000,
         isClosable: true,
-      });
+      })
     },
     onError(data, variables, context) {
-      console.log(data, "error");
+      console.log(data, 'error')
     },
-  });
+  })
 
   const { data: products } = useQuery({
     queryFn: getProducts,
-    queryKey: ["products"],
-  });
+    queryKey: ['products'],
+  })
 
-  console.log(products, "products");
+  console.log(products, 'products')
 
   return (
     <div>
       <Head>
-        <title>Admin | {t("products")}</title>
+        <title>Admin | {t('products')}</title>
         <link rel="icon" href="/admin6024190.png" />
       </Head>
 
@@ -85,19 +85,19 @@ function Login() {
           <AdminHeader />
           <Box display="flex" gap="28px">
             <AdminAsideMenu />
-            <AdminAsideMenuResponsive/>
+            <AdminAsideMenuResponsive />
             <AdminProductsSide />
           </Box>
         </Box>
       </Box>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Products
 
 export async function getStaticProps({ locale }: { locale: any }) {
   return {
-    props: { ...(await serverSideTranslations(locale, ["admin"])) },
-  };
+    props: { ...(await serverSideTranslations(locale, ['admin'])) },
+  }
 }
