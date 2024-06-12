5eeaa417ea00c06516019d48e9abc3534b7420c6
siriusgurban <failoglu96@gmail.com>
2024-06-12 22:51:25 +0400

    redesigned delete modal

--- shared/components/common/deleteModal/index.tsx
+++ shared/components/common/deleteModal/index.tsx
@@ -7,6 +7,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogOverlay,
+  Box,
   Button,
 } from '@chakra-ui/react'
 import React from 'react'
@@ -14,38 +15,44 @@ import { useTranslation } from 'react-i18next'
 
 function DeleteModal({ isOpen, onClose, handleDeleteConfirm }: any) {
   const { t } = useTranslation('admin')
+  const cancelRef = React.useRef<FocusableElemen>()
   return (
     <AlertDialog
       isOpen={isOpen}
-      // leastDestructiveRef={leastDestructiveRef}
+      leastDestructiveRef={cancelRef}
       onClose={onClose}
     >
       <AlertDialogOverlay>
-        <AlertDialogContent>
-          <AlertDialogHeader
-            fontSize="x-large"
-            fontWeight="bold"
-            className="bg-client-main-red text-center"
-          >
-            {t('DeleteProduct')}
-          </AlertDialogHeader>
-          <AlertDialogBody className="bg-white rounded-none text-center text-lg max-w-[300px] mx-auto text-client-main-gray2">
-            {t('AreYouSure')}
+        <AlertDialogContent className="pt-6 pb-10 ">
+          {/* <AlertDialogHeader></AlertDialogHeader> */}
+          <AlertDialogBody className="flex flex-col gap-2.5 rounded-none text-center text-lg mx-auto ">
+            <Box className="md:text-3xl xs:text-xl font-semibold max-w-[380px]">
+              {t('DeleteProduct')}
+            </Box>
+            <Box className="max-w-[280px] text-client-main-gray2 mx-auto md:text-lg xs:text-sm">
+              {t('AreYouSure')}
+            </Box>
+            <Box className="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col-reverse xs:flex-col-reverse mx-auto justify-center md:gap-7 sm:gap-2.5 xs:gap-2.5">
+              <Button
+                onClick={onClose}
+                rounded="none"
+                size="sm"
+                className="border border-client-main-gray1 md:w-24 xs:w-64 py-2 lowercase"
+              >
+                {t('Cancel')}
+              </Button>
+              <Button
+                colorScheme="red"
+                size="sm"
+                rounded="none"
+                onClick={handleDeleteConfirm}
+                className="md:w-24 xs:w-64 lowercase"
+              >
+                {t('Delete')}
+              </Button>
+            </Box>
           </AlertDialogBody>
-          <AlertDialogFooter display="flex" justifyContent="center" gap={5}>
-            <Button onClick={onClose} size="sm" rounded="none">
-              {t('Cancel')}
-            </Button>
-            <Button
-              colorScheme="red"
-              size="sm"
-              rounded="none"
-              onClick={handleDeleteConfirm}
-              ml={3}
-            >
-              {t('Delete')}
-            </Button>
-          </AlertDialogFooter>
+          {/* <AlertDialogFooter></AlertDialogFooter> */}
         </AlertDialogContent>
       </AlertDialogOverlay>
     </AlertDialog>
