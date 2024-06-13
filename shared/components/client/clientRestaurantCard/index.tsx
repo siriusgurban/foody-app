778b60cc3ed21202e67cc2c87b936f454ec229d9
siriusgurban <failoglu96@gmail.com>
2024-06-13 14:09:03 +0400

    client restaurant card image fixed

--- shared/components/client/clientRestaurantCard/index.tsx
+++ shared/components/client/clientRestaurantCard/index.tsx
@@ -21,31 +21,31 @@ function ClientRestaurantCard({ item, key }: { item: Product; key: number }) {
 
   return (
     <Box
-      className="md:w-60 w-32 flex flex-col shadow-lg px-2 pt-3 pb-2.5 md:px-4 md:pt-4 md:pb-6 cursor-pointer hover:rounded-md bg-white hover:brightness-75 transition-all hover:scale-105 duration-500 "
+      className="md:w-60 md:h-80 w-32 flex flex-col shadow-lg px-2 pt-3 pb-2.5 md:px-4 md:pt-4 md:pb-6 cursor-pointer hover:rounded-md bg-white hover:brightness-75 transition-all hover:scale-105 duration-500 "
       key={key}
       onClick={() => {
         push(CLIENT.RESTAURANTS + query?.id)
       }}
     >
-      {/* <Box className=" md:h-[140px] w-full h-20 mx-auto "> */}
+      {/* <Box className=" w-[174px] h-[160px] border border-2-black overflow-hidden m-auto"> */}
       <Image
-        width={184}
-        height={180}
+        width={174}
+        height={160}
         alt="card-iamge"
         src={item?.img_url}
-        className="md:mb-0 mb-1 object-cover my-auto"
+        className="xs:pb-0 md:pb-3 object-fill md:w-[174px] md:h-[160px] border border-2-black overflow-hidden m-auto"
       />
       {/* </Box> */}
       <Text className="text-center md:text-start md:text-xl text-base font-medium md:font-bold overflow-hidden w-24 h-6 md:mb-1">
         {item?.name}
       </Text>
-      <Box className="h-20">
-        <Text className="text-center md:text-start md:text-xl text-base mb-5 text-admin-restaurant-card-category">
+      <Box className="md:h-20 xs:h-14">
+        <Text className="text-center md:text-start md:text-base xs:text-xs mb-5 text-admin-restaurant-card-category">
           {restaurant?.data?.result?.data?.cuisine}
         </Text>
       </Box>
       <Box className="flex md:flex-row flex-col justify-between align-middle flex-wrap md:gap-0 gap-1">
-        <Text className="text-center text-xs md:text-base font-bold my-auto">
+        <Text className="text-center text-xs md:text-base font-bold my-auto text-client-main-gray2">
           ${restaurant?.data?.result?.data?.delivery_price} {t('Delivery')}
         </Text>
         <Box className="text-center text-[10px] md:text-base bg-client-main-red rounded-3xl text-white px-1 py-0 md:px-3 md:py-1">
